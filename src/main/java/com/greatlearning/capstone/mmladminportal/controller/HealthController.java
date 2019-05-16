d26faba66e50d332ac10c21818e5d6a839df0ca2
Kanchan Kumar Dey (kkumarde) <kkumarde@cisco.com>
2019-05-17 02:13:09 +0530

    updated buildspec

---
+++ src/main/java/com/greatlearning/capstone/mmladminportal/controller/HealthController.java
@@ -0,0 +1,51 @@
+/*
+ *
+ *  Copyright (c) 2018-2020 Givantha Kalansuriya, This source is a part of
+ *   Staxrt - sample application source code.
+ *   http://staxrt.com
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+
+package com.greatlearning.capstone.mmladminportal.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * The type User controller.
+ *
+ * @author Givantha Kalansuriya
+ */
+@RestController
+@RequestMapping("/")
+public class HealthController {
+
+ 
+  
+  /**
+   * Get all users list.
+   *
+   * @return the list
+   */
+  @GetMapping("/health")
+  public ResponseEntity<String> getAllUsers() {
+
+    return ResponseEntity.ok().body("<html><body>all good</body></html");
+  }
+
+ 
+}
--- src/main/java/com/greatlearning/capstone/mmladminportal/controller/UserController.java
+++ src/main/java/com/greatlearning/capstone/mmladminportal/controller/UserController.java
@@ -54,6 +54,7 @@ public class UserController {
   @Autowired
   private UserRepository userRepository;
 
+  
   /**
    * Get all users list.
    *
